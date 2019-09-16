282a3c65f939cdf282d594e8b4e70652c704af1f
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-09-16 12:35:23 +0300

    Добавьте файлы проекта.

---
+++ TestWebAppCIDI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestWebAppCIDI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
